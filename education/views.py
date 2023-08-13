0a85621d7d9c0d6168c036e46117e496e6c46bb3
sergey <szaprudnov@hotmail.com>
2023-08-13 22:11:29 +0300

    editing

--- education/views.py
+++ education/views.py
@@ -1,3 +1,33 @@
-from django.shortcuts import render
+from rest_framework.generics import CreateAPIView, DestroyAPIView, UpdateAPIView, RetrieveAPIView, ListAPIView
+from rest_framework.viewsets import ModelViewSet
 
-# Create your views here.
+from education.models import Well, Lesson
+from education.serializers import WellSerializer, LessonSerializer
+
+
+class WellViewSet(ModelViewSet):
+    serializer_class = WellSerializer
+    queryset = Well.objects.all()
+
+
+class WellCreateAPIView(CreateAPIView):
+    serializer_class = LessonSerializer
+
+
+class WellListAPIView(ListAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class WellDetailAPIView(RetrieveAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class WellUpdateAPIView(UpdateAPIView):
+    serializer_class = LessonSerializer
+    queryset = Lesson.objects.all()
+
+
+class WellDeleteAPIView(DestroyAPIView):
+    queryset = Lesson.objects.all()
