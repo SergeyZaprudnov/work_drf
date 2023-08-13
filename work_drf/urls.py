4097a6dac0b371ec3d6bffe2539c928d7bdd1e62
sergey <szaprudnov@hotmail.com>
2023-08-13 22:36:24 +0300

    editing

--- work_drf/urls.py
+++ work_drf/urls.py
@@ -22,7 +22,7 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('education.urls', namespace='education')),
-    path('users/', include('users.urls', namespace='users')),
+    path('user/', include('users.urls', namespace='user')),
 ]
 
 if settings.DEBUG:
