cdad59b113ce7fb411a7375e7e2717e627c3d004
sergey <szaprudnov@hotmail.com>
2023-08-13 21:47:27 +0300

    add class Well

--- education/models.py
+++ education/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {'blank': True, 'null': True}
+
+
+class Well(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Название')
+    preview = models.ImageField(upload_to='', **NULLABLE, verbose_name='Изображение')
+    description = models.CharField(max_length=150, verbose_name='Описание')
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+        ordering = ['title']
+
+    def __str__(self):
+        return f'{self.title}'
