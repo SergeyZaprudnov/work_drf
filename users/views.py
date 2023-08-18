b8fc4b0934941106e5f71a1fd09e4f41717d1d9c
sergey <szaprudnov@hotmail.com>
2023-08-18 23:47:30 +0300

    editing

--- users/views.py
+++ users/views.py
@@ -1,7 +1,10 @@
+from django.shortcuts import get_object_or_404
 from rest_framework.generics import UpdateAPIView, ListAPIView
+from rest_framework.permissions import IsAuthenticated
+from rest_framework.response import Response
 
 from users.models import User
-from users.serializers import UserSerializer
+from users.serializers import UserSerializer, UserLimitedSerializer
 
 
 class UserUpdateAPIView(UpdateAPIView):
@@ -12,3 +15,13 @@ class UserUpdateAPIView(UpdateAPIView):
 class UserListAPIView(ListAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+    permission_classes = [IsAuthenticated]
+
+    def retrieve(self, request, pk=None):
+        queryset = User.objects.all()
+        user = get_object_or_404(queryset, pk=pk)
+        if user == request.user:
+            serializer = UserSerializer(user)
+        else:
+            serializer = UserLimitedSerializer(user)
+        return Response(serializer.data)
