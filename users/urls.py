e9644042e23d9c900ad18326559c4a11b498cb51
sergey <szaprudnov@hotmail.com>
2023-08-20 06:49:59 +0300

    editing

--- users/urls.py
+++ users/urls.py
@@ -3,14 +3,18 @@ from django.urls import path
 from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
 
 from users.apps import UsersConfig
-from users.views import UserViewSet
+from users.views import *
 
 app_name = UsersConfig.name
 
-router = routers.DefaultRouter()
-router.register('', UserViewSet, basename='users')
-
 urlpatterns = [
-                  path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
-                  path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-              ] + router.urls
+    path('user/create/', UserCreateAPIView.as_view(), name='user-create'),
+    path('user/', UserListAPIView.as_view(), name='user-list'),
+    path('user/<int:pk>/', UserRetrieveAPIView.as_view(), name='user-get'),
+    path('user/update/<int:pk>/', UserUpdateAPIView.as_view(), name='user-update'),
+    path('user/delete/<int:pk>/', UserDestroyAPIView.as_view(), name='user-delete'),
+    path('user/payments/', UserPaymentsAPIView.as_view(), name='user-payments'),
+
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
+]
--- users/views.py
+++ users/views.py
@@ -1,23 +1,37 @@
-from django.shortcuts import get_object_or_404
-from rest_framework.generics import UpdateAPIView
-from rest_framework.response import Response
-from rest_framework.viewsets import ViewSet
+from rest_framework import generics
+from rest_framework.permissions import IsAuthenticated
 
 from users.models import User
 from users.serializers import UserSerializer
 
 
-class UserUpdateAPIView(UpdateAPIView):
-    queryset = User.objects.all()
+class UserCreateAPIView(generics.CreateAPIView):
     serializer_class = UserSerializer
 
 
-class UserViewSet(ViewSet):
+class UserListAPIView(generics.ListAPIView):
+    serializer_class = UserSerializer
     queryset = User.objects.all()
+
+
+class UserRetrieveAPIView(generics.RetrieveAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserUpdateAPIView(generics.UpdateAPIView):
+    serializer_class = UserSerializer
+    queryset = User.objects.all()
+
+
+class UserDestroyAPIView(generics.DestroyAPIView):
+    queryset = User.objects.all()
+
+
+class UserPaymentsAPIView(generics.RetrieveAPIView):
+    permission_classes = [IsAuthenticated]
+    # представлению
     serializer_class = UserSerializer
 
-    def retrieve(self, request, pk=None):
-        queryset = User.objects.all()
-        user = get_object_or_404(queryset, pk=pk)
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
+    def get_object(self):
+        return self.request.user
